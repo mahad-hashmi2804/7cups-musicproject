40adfb7555180be7dcb582e77c75986125608dea
Mahad Hashmi <mahad.python@gmail.com>
2023-08-25 17:03:31 +0800

    Added requirements.txt install command to build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,8 @@ set -o errexit
 
 poetry install
 
+pip install --upgrade pip
+pip install -r requirements.txt
+python manage.py makemigrations
 python manage.py collectstatic --no-input
 python manage.py migrate
