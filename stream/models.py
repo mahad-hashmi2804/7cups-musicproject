5441e5c4851b981e0dae335b91dced499be6a945
Mahad Hashmi <mahad.python@gmail.com>
2023-08-25 17:26:50 +0800

    attemping to fix django clash error

--- stream/models.py
+++ stream/models.py
@@ -1,5 +1,4 @@
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, AbstractUser
 from django.db import models
 
 
